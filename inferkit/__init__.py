ce67ed14e257ce586188c9ca7403c1a864c3a540
Mohammad Hossein <habibpour.programming@gmail.com>
2026-09-01 18:18:21 +0330

    bump 0.1.7

--- inferkit/__init__.py
+++ inferkit/__init__.py
@@ -3,4 +3,4 @@ from .helpers import bytes_to_response, image_to_base64
 from .server import create_app, serve
 
 __all__ = ["bytes_to_response", "create_app", "image_to_base64", "infer", "serve"]
-__version__ = "0.1.6"
+__version__ = "0.1.7"
